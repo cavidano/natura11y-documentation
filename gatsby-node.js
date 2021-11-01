71dce83c169a8067a21093ada2c6d029f1f84fb6
Carl Avidano <cavidano@gmail.com>
2021-11-01 13:46:24 -0400

    Create gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+exports.createPages = async ({ graphql, actions }) => {
+
+    const { data } = await graphql(`
+        query Docs {
+            allMdx {
+                nodes {
+                    slug
+                }
+            }
+        }
+    `);
+
+    data.allMdx.nodes.forEach((node) => {
+        actions.createPage({
+            path: '/docs' + node.slug,
+            component: path.resolve('./src/pages/project/docs'),
+            context: { slug: node.slug }
+        });
+    });
+
+}
