4dd955ba4f81d3ee694dabeacbaacbd2124578c5
Carl Avidano <cavidano@gmail.com>
2021-11-01 14:42:54 -0400

    Update documentation.js

--- src/templates/documentation.js
+++ src/templates/documentation.js
@@ -1,16 +1,34 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from '../components/Layout';
 
-function Documentation() {
+import { MDXRenderer} from 'gatsby-plugin-mdx';
+
+function Documentation({ data }) {
+    const { body } = data.mdx;
+    const { title } = data.mdx.frontmatter;
     return (
         <Layout>
-
-        <div>
-            <h1>Title</h1>
-        </div>
-            
+            <div>
+                <h1>{title}</h1>
+                <MDXRenderer>
+                    {body}
+                </MDXRenderer>
+            </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query queryDocPage($slug: String) {
+    mdx(slug: {eq: $slug}) {
+        body
+        frontmatter {
+          title
+        }
+      }
+  }
+`
+  
 
 export default Documentation;
