faa20cfe622a65d5cfd9cb881513140bbde331f2
Carl Avidano <cavidano@gmail.com>
2021-11-02 05:55:44 -0400

    before adding heros

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,19 +13,18 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-      },
-    },
-    {
+    `gatsby-transformer-sharp`, {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `docs`,
         path: `${__dirname}/src/docs/`,
       }
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
     }
   ]
 }
---
+++ src/components/docs/TextArea.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TextArea({children}) {
+    return (
+        <div className="container narrow margin-y-4">{children}</div>
+    )
+}
+
+export default TextArea;
