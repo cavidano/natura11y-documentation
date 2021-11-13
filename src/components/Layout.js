60b45ad1212e9b2ec5d72fbdcc7c17d2d78f1daa
Carl Avidano <cavidano@gmail.com>
2021-11-13 07:38:47 -0500

    adding index

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,44 +8,44 @@ import Footer from './Footer';
 
 import '../icons/natura11y-icons.css';
 
-import "../../natura11y/modules/accents";
-import "../../natura11y/modules/accents";
-import "../../natura11y/modules/article";
-import "../../natura11y/modules/aspect-ratios";
-import "../../natura11y/modules/backdrops";
-import "../../natura11y/modules/borders";
-import "../../natura11y/modules/breakpoints";
-import "../../natura11y/modules/cards";
-import "../../natura11y/modules/color";
-import "../../natura11y/modules/containers";
-import "../../natura11y/modules/display";
-import "../../natura11y/modules/flex";
-import "../../natura11y/modules/grid";
-import "../../natura11y/modules/icons";
-import "../../natura11y/modules/language";
-import "../../natura11y/modules/links";
-import "../../natura11y/modules/opacity";
-import "../../natura11y/modules/overflow";
-import "../../natura11y/modules/position";
-import "../../natura11y/modules/shadows";
-import "../../natura11y/modules/sizing";
-import "../../natura11y/modules/spacing";
-import "../../natura11y/modules/typography";
-import "../../natura11y/modules/z-index/index";
+import "../natura11y/modules/accents";
+import "../natura11y/modules/accents";
+import "../natura11y/modules/article";
+import "../natura11y/modules/aspect-ratios";
+import "../natura11y/modules/backdrops";
+import "../natura11y/modules/borders";
+import "../natura11y/modules/breakpoints";
+import "../natura11y/modules/cards";
+import "../natura11y/modules/color";
+import "../natura11y/modules/containers";
+import "../natura11y/modules/display";
+import "../natura11y/modules/flex";
+import "../natura11y/modules/grid";
+import "../natura11y/modules/icons";
+import "../natura11y/modules/language";
+import "../natura11y/modules/links";
+import "../natura11y/modules/opacity";
+import "../natura11y/modules/overflow";
+import "../natura11y/modules/position";
+import "../natura11y/modules/shadows";
+import "../natura11y/modules/sizing";
+import "../natura11y/modules/spacing";
+import "../natura11y/modules/typography";
+import "../natura11y/modules/z-index/index";
 
-import Accessibility from '../../natura11y/modules/accessibility';
-import Accordion from '../../natura11y/modules/accordion';
-import Alerts from '../../natura11y/modules/alerts';
-import Buttons from '../../natura11y/modules/buttons';
-import Collapse from '../../natura11y/modules/collapse';
-import Document from '../../natura11y/modules/document';
-import Forms from '../../natura11y/modules/forms';
-import Modal from '../../natura11y/modules/modal';
-import Navigation from '../../natura11y/modules/navigation';
-import Tabs from '../../natura11y/modules/tabs';
-import Tables from '../../natura11y/modules/tables';
+import Accessibility from '../natura11y/modules/accessibility';
+import Accordion from '../natura11y/modules/accordion';
+import Alerts from '../natura11y/modules/alerts';
+import Buttons from '../natura11y/modules/buttons';
+import Collapse from '../natura11y/modules/collapse';
+import Document from '../natura11y/modules/document';
+import Forms from '../natura11y/modules/forms';
+import Modal from '../natura11y/modules/modal';
+import Navigation from '../natura11y/modules/navigation';
+import Tabs from '../natura11y/modules/tabs';
+import Tables from '../natura11y/modules/tables';
 
-import "../../natura11y/modules/theme";
+import "../natura11y/modules/theme";
 
 function Layout({ children }) {
 
@@ -83,7 +83,6 @@ function Layout({ children }) {
       
       <Helmet>
         <title>{title}</title>
-        <link rel="stylesheet" href="/prism.css" rel="stylesheet" />
       </Helmet>
 
       <Header version={version} />
--- src/components/docs/FigureBlock.js
+++ src/components/docs/FigureBlock.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 
-import Prism from 'prismjs';
-import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
-import 'prismjs/plugins/line-highlight/prism-line-highlight';
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
-import 'prismjs/plugins/toolbar/prism-toolbar';
-import 'prismjs/plugins/show-language/prism-show-language';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
-import 'prismjs/plugins/treeview/prism-treeview';
+import Prism from '../../prismjs/prism';
+
+import '../../prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
+import '../../prismjs/plugins/line-highlight/prism-line-highlight';
+import '../../prismjs/plugins/line-numbers/prism-line-numbers';
+import '../../prismjs/plugins/toolbar/prism-toolbar';
+import '../../prismjs/plugins/show-language/prism-show-language';
+import '../../prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import '../../prismjs/plugins/treeview/prism-treeview';
+
+import '../../prismjs/themes/prism-carl.css';
 
 import Pre from './Pre';
 
