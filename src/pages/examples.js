e096d18d3be51e0a8a3d9c5c90569d25fc78c136
Carl Avidano <cavidano@gmail.com>
2021-11-13 02:25:23 -0500

    before adding doc content

---
+++ src/pages/examples.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const Examples = () => {
+    return (
+        <Layout>
+            <h2>EXAMPLES</h2>
+        </Layout>
+    )
+}
+
+export default Examples;
