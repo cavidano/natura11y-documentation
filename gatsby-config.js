d7fd4cdef276fdfd25eb0c81ae618faf6f6d8a3e
Carl Avidano <cavidano@gmail.com>
2021-12-12 05:26:56 -0500

    before getting images in mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
               linkImagesToOriginal: false,
               quality: 90
             },
-          },
+          }
         ],
       },
     },
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,6 @@ import { Link } from "gatsby";
 
 const Header = ({ version }) => {
 
-  if (window.location.href.indexOf("/docs") > -1) {
-    // alert("You are in the docs");
-  }
-
   const logo = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
