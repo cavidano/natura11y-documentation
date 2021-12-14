0910da5d71a9577e6a7ac1b9f53ec3b00658be82
Carl Avidano <cavidano@gmail.com>
2021-12-14 17:12:22 -0500

    adding color doc

--- src/components/docs/examples/Color/ThemeColors.js
+++ src/components/docs/examples/Color/ThemeColors.js
@@ -1,1 +1,26 @@
-rfce    
+import React, { Fragment } from 'react';
+
+import { exampleStyles } from '../../../../mdxVars';
+
+import ThemeCard from './ThemeCard';
+
+function ThemeColors() {
+
+    return (
+        <div className="container narrow grid gap-3 margin-y-4" style={exampleStyles}>
+
+            <ThemeCard themeClass="theme-primary" border={false} />
+
+            <ThemeCard themeClass="theme-secondary" border={false} />
+
+            <ThemeCard themeClass="theme-dark" border={false} />
+
+            <ThemeCard themeClass="theme-light" border={false} />
+
+            <ThemeCard />
+
+        </div>            
+    )
+}
+
+export default ThemeColors;
