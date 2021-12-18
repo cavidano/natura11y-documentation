e71fe4e89c2ee08571c6845c8196af484b4ac029
Carl Avidano <cavidano@gmail.com>
2021-12-18 10:13:46 -0500

    adding grid doc

---
+++ src/components/docs/examples/Grids/MatrixExample.js
@@ -0,0 +1,167 @@
+import React from 'react';
+
+import { exampleStyles } from '../../../../mdxVars';
+
+const MatrixExample = () => {
+    return (
+        <figure className="container medium margin-y-4">
+
+            <div className="matrix matrix--cell-fill gap-border text-align-center font-size-md" style={exampleStyles}>
+
+                <div className="matrix__cell matrix--cell-1">
+                    <div className="padding-y-2 theme-primary">
+                        <p>1</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-11">
+                    <div className="padding-y-2 theme-primary">
+                        <p>11</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-2">
+                    <div className="padding-y-2 theme-primary">
+                        <p>2</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-10">
+                    <div className="padding-y-2 theme-primary">
+                        <p>10</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-3">
+                    <div className="padding-y-2 theme-primary">
+                        <p>3</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-9">
+                    <div className="padding-y-2 theme-primary">
+                        <p>9</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-4">
+                    <div className="padding-y-2 theme-primary">
+                        <p>4</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-8">
+                    <div className="padding-y-2 theme-primary">
+                        <p>8</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-5">
+                    <div className="padding-y-2 theme-primary">
+                        <p>5</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-7">
+                    <div className="padding-y-2 theme-primary">
+                        <p>7</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-6">
+                    <div className="padding-y-2 theme-primary">
+                        <p>6</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-6">
+                    <div className="padding-y-2 theme-primary">
+                        <p>6</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-4">
+                    <div className="padding-y-2 theme-primary">
+                        <p>4</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-4">
+                    <div className="padding-y-2 theme-primary">
+                        <p>4</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-4">
+                    <div className="padding-y-2 theme-primary">
+                        <p>4</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-3">
+                    <div className="padding-y-2 theme-primary">
+                        <p>3</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-3">
+                    <div className="padding-y-2 theme-primary">
+                        <p>3</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-3">
+                    <div className="padding-y-2 theme-primary">
+                        <p>3</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-3">
+                    <div className="padding-y-2 theme-primary">
+                        <p>3</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-2">
+                    <div className="padding-y-2 theme-primary">
+                        <p>2</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-2">
+                    <div className="padding-y-2 theme-primary">
+                        <p>2</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-2">
+                    <div className="padding-y-2 theme-primary">
+                        <p>2</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-2">
+                    <div className="padding-y-2 theme-primary">
+                        <p>2</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-2">
+                    <div className="padding-y-2 theme-primary">
+                        <p>2</p>
+                    </div>
+                </div>
+
+                <div className="matrix__cell matrix--cell-2">
+                    <div className="padding-y-2 theme-primary">
+                        <p>2</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </figure>
+    )
+};
+
+export default MatrixExample;
