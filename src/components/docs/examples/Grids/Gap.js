58df57a6223587993dcfc37089ca3e7e7cf264e3
Carl Avidano <cavidano@gmail.com>
2021-12-19 05:31:13 -0500

    Update Gap.js

--- src/components/docs/examples/Grids/Gap.js
+++ src/components/docs/examples/Grids/Gap.js
@@ -10,11 +10,11 @@ const Gap = () => {
         <div className="grid gap-3">
 
             <div>
-                <p class="font-size-md margin-bottom-2">
+                <p className="font-size-md margin-bottom-2">
                     The <code>.matrix</code> selector with <code>.gap-1</code> applied:
                 </p>
 
-                <div class="matrix matrix--cell-4 gap-1 text-align-center font-size-md">
+                <div className="matrix matrix--cell-4 gap-1 text-align-center font-size-md">
 
                     <MatrixCell label="01" />
                     <MatrixCell label="02" />
@@ -29,11 +29,11 @@ const Gap = () => {
 
             <div>
             
-                <p class="font-size-md margin-bottom-2">
+                <p className="font-size-md margin-bottom-2">
                     The <code>.grid</code> selector with <code>.gap-2</code> applied:
                 </p>
 
-                <div class="grid grid--column-3 gap-2">
+                <div className="grid grid--column-3 gap-2">
 
                     <GridItem label="01" />
                     <GridItem label="02" />
