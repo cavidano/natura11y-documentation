556f049b0089d39bdd6dfa652080fd23ab865b49
Carl Avidano <cavidano@gmail.com>
2021-12-21 09:15:37 -0500

    adding opacities

---
+++ src/components/docs/examples/Opacities/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+const Opacities = () => {
+    return (
+        <div className="grid grid--column-3 gap-2 text-align-center font-size-sm">
+
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-10"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-10</code></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-20"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-20</code></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-30"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-30</code></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-40"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-40</code></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-50"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-50</code></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-60"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-60</code></p>
+                    </div>
+                </div>
+            </div>
+                                
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-70"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-70</code></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-80"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-80</code></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="position-relative overflow-hidden">
+                <div className="aspect-ratio-3x2 theme-primary opacity-90"></div>
+                <div className="position-bottom-left flex-row justify-content-center">                
+                    <div className="padding-1 box-shadow-1 border-radius-top theme-white">
+                        <p><code>.opacity-90</code></p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Opacities;
