f97e78e64541ce7ff6858b7dae28e3ff41d6b820
Carl Avidano <cavidano@gmail.com>
2021-12-22 05:36:28 -0500

    adding position doc

---
+++ src/components/docs/examples/Position/Absolute.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Absolute = () => {
+
+    const setHeight = {
+        'height': '240px'
+    }
+
+    return (
+        <div className="container narrow font-size-md">
+                    
+            <div className="position-relative subtle-fill margin-bottom-4" style={setHeight}>
+                <div className="position-top-left padding-x-3 padding-y-2 theme-white border">
+                    <p><code>.position-absolute</code></p>
+                </div>
+            </div>
+            
+            <div className="position-relative subtle-fill margin-bottom-4" style={setHeight}>
+                <div className="position-top-right padding-x-3 padding-y-2 theme-white border">
+                    <p><code>.position-top-right</code></p>
+                </div>
+            </div>
+
+            <div className="position-relative subtle-fill margin-bottom-4" style={setHeight}>
+                <div className="position-bottom-left padding-x-3 padding-y-2 theme-white border">
+                    <p><code>.position-bottom-left</code></p>
+                </div>
+            </div>
+
+            <div className="position-relative subtle-fill" style={setHeight}>
+                <div className="position-bottom-right padding-x-3 padding-y-2 theme-white border">
+                    <p><code>.position-bottom-right</code></p>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Absolute;
