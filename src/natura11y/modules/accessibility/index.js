84312881e44def0dabb2a94b045d18d69e42cf16
Carl Avidano <cavidano@gmail.com>
2021-12-31 07:18:08 -0500

    cleaning up js

--- src/natura11y/modules/accessibility/index.js
+++ src/natura11y/modules/accessibility/index.js
@@ -5,37 +5,37 @@ import "./_style.scss"
 //////////////////////////////////////////////
 
 export default class Accessibility {
+
   constructor() {
+  
     const elements = [
-      "a[href]",
-      "button",
+      'a[href]',
+      'button',
       '[role="tab"]',
       '[data-toggle="accordion"]',
-    ]
+    ];
 
-    const initFocus = () => {
-      const focusableElementList = document.querySelectorAll(elements)
+    const focusableElementList = document.querySelectorAll(elements);
 
-      focusableElementList.forEach(focusableElement => {
-        let mouseDown = false
+    focusableElementList.forEach(focusableElement => {
+      let mouseDown = false
 
-        focusableElement.addEventListener("mousedown", () => {
-          mouseDown = true
-        })
+      focusableElement.addEventListener('mousedown', () => {
+        mouseDown = true
+      });
 
-        focusableElement.addEventListener("mouseup", () => {
-          mouseDown = false
-        })
+      focusableElement.addEventListener('mouseup', () => {
+        mouseDown = false
+      });
 
-        focusableElement.addEventListener("focus", event => {
-          if (mouseDown) {
-            event.target.blur()
-          }
-        })
-      })
-    }
-    
-    initFocus();
+      focusableElement.addEventListener('focus', event => {
+        if (mouseDown) {
+          event.target.blur()
+        }
+      });
+
+    });
 
   }
+  
 }
