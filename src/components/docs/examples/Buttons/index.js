c28048ca15ee6ca7ad91251b07b4dc545c8f8bfa
Carl Avidano <cavidano@gmail.com>
2022-01-02 03:16:38 -0500

    Update index.js

--- src/components/docs/examples/Buttons/index.js
+++ src/components/docs/examples/Buttons/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const ButtonSelector = () => {
+const ButtonSelector = (props) => {
+    
+    const { buttonClasses } = props;
+
     return (
         <div className="text-align-center">
             <a className="button" href="#1">
@@ -8,6 +11,10 @@ const ButtonSelector = () => {
             </a>
         </div>
     );
+}
+
+ButtonSelector.defaultProps = {
+    buttonClasses = 'button'
 }
 
 export default ButtonSelector;
