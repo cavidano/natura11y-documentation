c5c983e7ecdf7396fa8ccd1f1c5bfae8d2210e7e
Carl Avidano <cavidano@gmail.com>
2022-01-02 11:09:01 -0500

    adding new examples

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -64,53 +64,6 @@ const Examples = () => {
                 </div>
 
             </section>
-            <hr />
-
-            <section className='container medium margin-y-5'>
-
-                <div className="grid grid--column-2--lg align-items-center box-shadow-3">
-                    
-                    <StaticImage
-                        src="../images/examples/template-example-01.jpg"
-                        alt=""
-                        placeholder="dominantColor"
-                        layout="fullWidth"
-                    />
-
-                    <div className="padding-4 text-align-center">
-
-                        <h5>Landing Page</h5>
-
-                        <p>
-                            A simple landing page with global navigation below the logo.
-                        </p>
-
-                        <nav className="font-size-sm text-color-link">
-
-                            <ul className="nav nav--horizontal justify-content-center">
-                                <li>
-                                    <a
-                                        className="button button--outline button--has-icon"
-                                        href="https://github.com/cavidano/natura11y/blob/main/dist/html/layouts/landing-page.html"
-                                        target="_blank">
-                                        <span className="icon icon-github button__icon"></span>
-                                        <span className="button__text">View on GitHub</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://cavidano.github.io/natura11y/dist/html/layouts/landing-page.html" target="_blank">
-                                        Preview in Browser
-                                    </a>
-                                </li>
-                            </ul>
-
-                        </nav>
-
-                    </div>
-
-                </div>
-
-            </section>
 
         </Layout>
     )
