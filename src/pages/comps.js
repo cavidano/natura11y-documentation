874ddb37cd1fa34cd87ea62dbba84f1aa83c9350
Carl Avidano <cavidano@gmail.com>
2022-01-18 03:41:41 -0500

    cleaning up comps layout

--- src/pages/comps.js
+++ src/pages/comps.js
@@ -21,47 +21,43 @@ const Comps = () => {
 
             <section className='container wide padding-y-5'>
 
-                <div className="theme-primary" style={{'--primary': '#FFDB26', '--primary-text':'black'}}>
-
-                    <div className="grid grid--column-2--lg align-items-center box-shadow-2">
+                <div className="grid grid--column-2--lg align-items-center theme-white box-shadow-2">
                         
-                        <StaticImage
-                            src="../images/examples/comp-thumb-call-to-action.jpg"
-                            alt=""
-                            placeholder="dominantColor"
-                            layout="fullWidth"
-                        />
+                    <StaticImage
+                        src="../images/examples/comp-thumb-call-to-action.jpg"
+                        alt="Thumbnail of Natura11y's Call to Action landing page"
+                        placeholder="dominantColor"
+                        layout="fullWidth"
+                    />
 
-                        <div className="padding-4 text-align-center">
+                    <div className="padding-4 text-align-center">
 
-                            <h2 className='h4'>Call to Action</h2>
+                        <h2 className='h4'>Call to Action</h2>
 
-                            <p>
-                                A simple landing page with global navigation below the logo.
-                            </p>
+                        <p>
+                            This simple landing page demonstrates mixed uses of Natura11y's <a href="/docs/backdrops">backdrop</a> and <a href="/docs/grids/#cssGrid">CSS grid</a> components.
+                        </p>
 
-                            <nav className="font-size-sm text-color-link">
+                        <nav className="font-size-sm text-color-link">
 
-                                <ul className="nav nav--horizontal justify-content-center">
-                                    <li>
-                                        <a href="https://cavidano.github.io/natura11y/dist/html/comps/call-to-action" target="_blank">
-                                            Preview in Browser
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            className="button button--outline button--has-icon"
-                                            href="https://github.com/cavidano/natura11y/blob/main/dist/html/comps/call-to-action/index.html"
-                                            target="_blank">
-                                            <span className="icon icon-github button__icon"></span>
-                                            <span className="button__text">View on GitHub</span>
-                                        </a>
-                                    </li>
-                                </ul>
+                            <ul className="nav nav--horizontal justify-content-center">
+                                <li>
+                                    <a href="https://cavidano.github.io/natura11y/dist/html/comps/call-to-action" target="_blank">
+                                        Preview in Browser
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className="button button--outline button--has-icon"
+                                        href="https://github.com/cavidano/natura11y/blob/main/dist/html/comps/call-to-action/index.html"
+                                        target="_blank">
+                                        <span className="icon icon-github button__icon"></span>
+                                        <span className="button__text">View on GitHub</span>
+                                    </a>
+                                </li>
+                            </ul>
 
-                            </nav>
-
-                        </div>
+                        </nav>
 
                     </div>
 
