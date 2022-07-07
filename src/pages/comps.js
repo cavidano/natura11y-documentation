3b0d1e6f827efb8c289bd1767b85ef5065f62cfb
Carl Avidano <cavidano@gmail.com>
2022-07-07 10:06:52 -0400

    added second comp

--- src/pages/comps.js
+++ src/pages/comps.js
@@ -20,7 +20,7 @@ const Comps = () => {
                     Explore free-to-use comprehensive layouts.
                 </h2>
                 <p>
-                    The templates below are built using Natura11y. Use them as they are or cherry pick the parts you want.
+                    The layouts below are built using Natura11y. Use them as they are or cherry pick the parts you want.
                 </p>
             </section>
 
@@ -40,7 +40,7 @@ const Comps = () => {
 
                         <div className="padding-4 text-align-center">
 
-                            <h2 className='h4'>Landing Page</h2>
+                            <h2 className='h4'>Homepage</h2>
 
                             <p>
                                 This simple landing page demonstrates mixed uses of Natura11y's <a href="/docs/backdrops">backdrop</a> and <a href="/docs/grids/#cssGrid">CSS grid</a> components.
@@ -50,14 +50,65 @@ const Comps = () => {
 
                                 <ul className="nav nav--horizontal justify-content-center">
                                     <li>
-                                        <a href="https://cavidano.github.io/natura11y/dist/html/comps/birds-landing-page/" target="_blank">
+                                        <a href="https://cavidano.github.io/natura11y/dist/html/comps/bird-landing-page/" target="_blank">
                                             Preview in Browser
                                         </a>
                                     </li>
                                     <li>
                                         <a
                                             className="button button--outline button--has-icon"
-                                            href="https://github.com/cavidano/natura11y/blob/main/dist/html/comps/birds-landing-page/index.html"
+                                            href="https://github.com/cavidano/natura11y/blob/main/dist/html/comps/bird-landing-page/index.html"
+                                            target="_blank">
+                                                <span className="icon icon-github button__icon"></span>
+                                                <span className="button__text">View on GitHub</span>
+                                        </a>
+                                    </li>
+                                </ul>
+
+                            </nav>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            <hr />
+
+            <section className="padding-y-5" style={{'--primary': '#1d1920'}}>
+
+                <div className="container wide">
+                
+                    <div className="grid grid--column-2--lg align-items-center">
+                            
+                        <StaticImage
+                            className="box-shadoZw-2"
+                            src="../images/examples/comps-lion-article-page.jpg"
+                            alt="Thumbnail of Natura11y's Call to Action landing page"
+                        />
+
+                        <div className="padding-4 text-align-center">
+
+                            <h2 className='h4'>Article Page</h2>
+
+                            <p>
+                                This article page example uses various <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path" target="_blank">clip path</a> properties to acheive a custom look.
+                            </p>
+
+                            <nav className="font-size-sm text-color-link">
+
+                                <ul className="nav nav--horizontal justify-content-center">
+                                    <li>
+                                        <a href="https://cavidano.github.io/natura11y/dist/html/comps/lion-article-page/" target="_blank">
+                                            Preview in Browser
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="button button--outline button--has-icon"
+                                            href="https://github.com/cavidano/natura11y/blob/main/dist/html/comps/lion-article-page/index.html"
                                             target="_blank">
                                                 <span className="icon icon-github button__icon"></span>
                                                 <span className="button__text">View on GitHub</span>
