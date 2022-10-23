150f1ed1e25bc92d6c9a5be0d4dae589196163eb
Carl Avidano <cavidano@gmail.com>
2022-10-23 07:03:31 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    globals: {
-      __PATH_PREFIX__: true,
-    },
-    extends: `react-app`,
-    rules: {
-        "import/first": 0,
-        "import/order": 0
-    }
-  }
+	globals: {
+		__PATH_PREFIX__: true,
+	},
+	extends: `react-app`,
+	rules: {
+		'import/first': 0,
+		'import/order': 0,
+	},
+};
